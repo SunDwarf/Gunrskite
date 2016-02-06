10e06556f56ca4831a26586b194e317a2760bf52
SunDwarf <eyesismine@gmail.com>
2016-02-06 22:40:56 +0000

    Initial commit

---
+++ udpserver.py
@@ -0,0 +1,54 @@
+"""
+Gunrskite UDP Logging Server
+
+Binds to 0.0.0.0:23887 by default.
+"""
+import asyncio
+import logging
+import argparse
+
+loop = asyncio.get_event_loop()
+
+formatter = logging.Formatter('%(asctime)s - [%(levelname)s] %(name)s -> %(message)s')
+root = logging.getLogger()
+
+root.setLevel(logging.DEBUG)
+
+consoleHandler = logging.StreamHandler()
+consoleHandler.setFormatter(formatter)
+root.addHandler(consoleHandler)
+
+logger = logging.getLogger("Gunrskite-udp")
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-b", "--bind", help="IP address to bind to", default="0.0.0.0")
+parser.add_argument("-p", "--port", help="Port to bind to", default=23887, type=int)
+args = parser.parse_args()
+
+
+class LoggerProtocol(object):
+    def connection_made(self, transport):
+        self.transport = transport
+
+    def datagram_received(self, data, addr):
+        message = data.decode()
+        logger.info("Recieved message {}".format(message))
+
+
+def __main__():
+    logger.info("Gunrskite logging server loading")
+    logger.info("Binding on UDP to {}:{}".format(args.bind, args.port))
+    listen_server = loop.create_datagram_endpoint(
+        LoggerProtocol, local_addr=(args.bind, args.port))
+    transport, protocol = loop.run_until_complete(listen_server)
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+
+    transport.close()
+    loop.close()
+
+if __name__ == "__main__":
+    __main__()
