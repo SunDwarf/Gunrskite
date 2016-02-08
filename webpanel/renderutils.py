20999a3e2968b379375527afa22978d07fd25e97
SunDwarf <eyesismine@gmail.com>
2016-02-08 21:47:42 +0000

    Add top_player display to server list

--- webpanel/renderutils.py
+++ webpanel/renderutils.py
@@ -1,8 +1,18 @@
 """
 Rendering utilities.
 """
+from sqlalchemy import desc
+
 from gunrskite import db
 
 
 def get_top_player(server: db.Server):
-    players
+    player = db.db.session.query(db.ServerUser) \
+        .join(db.ServerUser.server)\
+        .filter(db.ServerUser.server == server)\
+        .order_by(desc(db.ServerUser.points))\
+        .limit(1)\
+        .first()
+
+
+    return player
--- webpanel/routes.py
+++ webpanel/routes.py
@@ -3,7 +3,7 @@ Routes for the app.
 """
 from flask import Blueprint, render_template
 from gunrskite import db
-from webpanel import srcds
+from webpanel import srcds, renderutils
 
 routes_bp = Blueprint("gunrskite", __name__)
 
@@ -11,5 +11,5 @@ routes_bp = Blueprint("gunrskite", __name__)
 @routes_bp.route("/")
 def index():
     servers_s = db.db.session.query(db.Server).all()
-    servers = [srcds.get_server_info(serv.ip, serv.port) for serv in servers_s]
-    return render_template("index.html", servers=servers)
+    servers = [(srcds.get_server_info(serv.ip, serv.port), serv) for serv in servers_s]
+    return render_template("index.html", servers=servers, renderutils=renderutils)
